6fd9e826a7a43f5b2d9f4792c8d62decad72fb77
JMartinTorres <jmartintorres@outlook.es>
2020-10-20 21:15:33 +0200

    Initialize extension on first installation

--- hi_barbie/content_scripts/hibarbie.js
+++ hi_barbie/content_scripts/hibarbie.js
@@ -1,10 +1,11 @@
 const BARBIE_GIRL_YOUTUBE_URL = 'https://youtu.be/ZyhrYis509A?t=15';
 
-$(document).ready(function() {
-		
+$(document).ready(barbifyAllLinks);
+
+function barbifyAllLinks() {
 	// Once the document is loaded, all the <a> elements it initially contains are assigned the function that redirects to Barbie Girl video as a click handler. 
-	$("a").click(function() {
-		forgetItGoPlayBarbieGirl();
+	$("a").click(function(event) {
+		forgetItGoPlayBarbieGirl(event);
 	});
 
 	// Since new <a> elements added to the page are wanted to behave the same way, this MutationObserver
@@ -12,17 +13,18 @@ $(document).ready(function() {
 	var mutationObs = new MutationObserver(function () {
 		$("a:not(.barbified-link)").each(function () {
 			$(this).addClass("barbified-link");
-			$(this).click(forgetItGoPlayBarbieGirl);
+			$(this).click(function(event) {
+				forgetItGoPlayBarbieGirl(event);
+			});
 		});
 	});
 
 	// Initialize observation
 	mutationObs.observe(document.body, { childList: true, subtree: true });
-	
-});
+}
 
 // Remove the default actions of clicked elements and go to Barbie Girl YouTube video
-function forgetItGoPlayBarbieGirl() {
-	event.preventDefault();
+function forgetItGoPlayBarbieGirl(e) {
+	e.preventDefault();
 	window.location.assign(BARBIE_GIRL_YOUTUBE_URL);
 }
