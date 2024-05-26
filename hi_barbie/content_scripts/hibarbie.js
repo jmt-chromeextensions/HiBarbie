aa749194684610975cdb3cad9ef7667180cfd0de
JMartinTorres <jmartintorres@outlook.es>
2024-05-26 19:34:48 +0200

    simplification-experiment-not-working

--- hi_barbie/content_scripts/hibarbie.js
+++ hi_barbie/content_scripts/hibarbie.js
@@ -1,30 +1,27 @@
 const BARBIE_GIRL_YOUTUBE_URL = 'https://youtu.be/ZyhrYis509A?t=15';
 
-$(document).ready(barbifyAllLinks);
+// $(document).ready(barbifyAllLinks);
 
-function barbifyAllLinks() {
-	// Once the document is loaded, all the <a> elements it initially contains are assigned the function that redirects to Barbie Girl video as a click handler. 
-	$("a").click(function(event) {
-		forgetItGoPlayBarbieGirl(event);
+setInterval(() => {
+	$("a:not(.barbified-link)").each(function () {
+		$(this).addClass("barbified-link").attr("href", BARBIE_GIRL_YOUTUBE_URL);
 	});
+}, 100);
 
-	// Since new <a> elements added to the page are wanted to behave the same way, this MutationObserver
-	// is used to detect when they are included so the click handler can be assigned to them too.
-	var mutationObs = new MutationObserver(function () {
-		$("a:not(.barbified-link)").each(function () {
-			$(this).addClass("barbified-link");
-			$(this).click(function(event) {
-				forgetItGoPlayBarbieGirl(event);
-			});
-		});
-	});
+// function barbifyAllLinks() {
+// 	// Once the document is loaded, all the <a> elements it initially contains are assigned the function that redirects to Barbie Girl video as a click handler. 
+// 	$("a").click(function(event) {
+// 		forgetItGoPlayBarbieGirl(event);
+// 	});
 
-	// Initialize observation
-	mutationObs.observe(document.body, { childList: true, subtree: true });
-}
+// 	// Since new <a> elements added to the page are wanted to behave the same way, this MutationObserver
+// 	// is used to detect when they are included so the click handler can be assigned to them too.
+// 	var mutationObs = new MutationObserver(function () {
+// 		$("a:not(.barbified-link)").each(function () {
+// 			$(this).addClass("barbified-link").attr("href", BARBIE_GIRL_YOUTUBE_URL);
+// 		});
+// 	});
 
-// Remove the default actions of clicked elements and go to Barbie Girl YouTube video
-function forgetItGoPlayBarbieGirl(e) {
-	e.preventDefault();
-	window.location.assign(BARBIE_GIRL_YOUTUBE_URL);
-}
+// 	// Initialize observation
+// 	mutationObs.observe(document.body, { childList: true, subtree: true });
+// }
